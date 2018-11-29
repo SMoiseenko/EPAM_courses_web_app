01f051ce9921489b6d8941bd21b84f5ab8797a63
s-moiseenko <s-moiseenko@MyLaptop>
2018-11-30 00:39:58 +0300

    New database backup.

--- mysql_dump/mysql_dump.sql
+++ mysql_dump/mysql_dump.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `periodicalsDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `periodicalsDB`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Linux (x86_64)
 --
--- Host: localhost    Database: periodicalsDB
+-- Host: localhost    Database: mytestDB
 -- ------------------------------------------------------
 -- Server version	8.0.13
 
@@ -18,29 +16,73 @@ USE `periodicalsDB`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `admin`
+-- Current Database: `mytestDB`
 --
 
-DROP TABLE IF EXISTS `admin`;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `mytestDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `mytestDB`;
+
+--
+-- Table structure for table `e_mail`
+--
+
+DROP TABLE IF EXISTS `e_mail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `admin` (
+CREATE TABLE `e_mail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `email` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin`
+-- Dumping data for table `e_mail`
 --
 
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'root','root'),(2,'customer','1234');
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+LOCK TABLES `e_mail` WRITE;
+/*!40000 ALTER TABLE `e_mail` DISABLE KEYS */;
+INSERT INTO `e_mail` VALUES (1,'ivanov@tut.by'),(4,'nikitin@tut.by'),(2,'pietrov@tut.by'),(3,'sidorov@tut.by'),(5,'slipknotbiocom@gmail.com');
+/*!40000 ALTER TABLE `e_mail` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `age` int(11) DEFAULT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `e_mail_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `age` (`age`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,15,'ivan','ivanov',1),(2,52,'pietr','pietrov',2),(3,87,'sidor','sidorov',3),(4,4,'nikita','nikitin',4),(5,33,'Сергей','Моисеенко',5);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `periodicalsDB`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `periodicalsDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `periodicalsDB`;
 
 --
 -- Table structure for table `newspapers`
@@ -50,14 +92,14 @@ DROP TABLE IF EXISTS `newspapers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `newspapers` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ISSN` varchar(9) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` text,
   `qty_per_year` int(11) NOT NULL,
   `price` decimal(5,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,8 +108,70 @@ CREATE TABLE `newspapers` (
 
 LOCK TABLES `newspapers` WRITE;
 /*!40000 ALTER TABLE `newspapers` DISABLE KEYS */;
-INSERT INTO `newspapers` VALUES ('1','0190-8286','The Washington Post','The Washington Post (sometimes abbreviated as WaPo) is a major American daily newspaper published in Washington, D.C., with a particular emphasis on national politics and the federal government. It has the largest circulation in the Washington metropolitan area. Its slogan \"Democracy Dies in Darkness\" began appearing on its masthead in 2017. Daily broadsheet editions are printed for the District of Columbia, Maryland, and Virginia.',365,0.35),('2','0362-4331','The New York Times','The New York Times (shortened as The Times and abbreviated as The NYT) is an American newspaper based in New York City with worldwide influence and readership.Founded in 1851, the paper has won 125 Pulitzer Prizes, more than any other newspaper. The Times is ranked 17th in the world by circulation and 2nd in the U.S. ',365,0.50),('3','1','1','1',1,1.00),('4','1234-123','TEST','TEst Discription',666,123.00),('5','1234-5623','TEST','TEst Discription',666,123.00),('6','1234-5678','TEST','TEst Discription',666,1.20),('7','mobile','testew','will it wirk',625,0.27);
+INSERT INTO `newspapers` VALUES (1,'0190-8286','The Washington Post','The Washington Post (sometimes abbreviated as WaPo) is a major American daily newspaper published in Washington, D.C., with a particular emphasis on national politics and the federal government. It has the largest circulation in the Washington metropolitan area. Its slogan \"Democracy Dies in Darkness\" began appearing on its masthead in 2017. Daily broadsheet editions are printed for the District of Columbia, Maryland, and Virginia.',365,0.35),(2,'0362-4331','The New York Times','The New York Times (shortened as The Times and abbreviated as The NYT) is an American newspaper based in New York City with worldwide influence and readership.Founded in 1851, the paper has won 125 Pulitzer Prizes, more than any other newspaper. The Times is ranked 17th in the world by circulation and 2nd in the U.S. ',365,0.50),(3,'1','1','1',1,1.00),(4,'1234-123','TEST','TEst Discription',666,123.00),(5,'1234-5623','TEST','TEst Discription',666,123.00),(6,'1234-5678','TEST','TEst Discription',666,1.20),(7,'mobile','testew','will it wirk',625,0.27);
 /*!40000 ALTER TABLE `newspapers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(40) NOT NULL DEFAULT 'anonymous',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin',2),(2,'customer',2),(3,'anonymous',2);
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `first_name` varchar(40) NOT NULL,
+  `last_name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `adress` text NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`),
+  KEY `user_fk0` (`role_id`),
+  CONSTRAINT `user_fk0` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','admin',1,'Admin','Adminovich','admin@admin.com','+375295556666','230025, Grodno, Kirova str., 1 ',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -79,4 +183,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-28  0:01:33
+-- Dump completed on 2018-11-30  0:38:48

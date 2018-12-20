699d9afa6886bf019e54f74b53ae0174fc8819d9
s-moiseenko <s-moiseenko@MyLaptop>
2018-12-20 16:25:09 +0300

    MySQL dump.

--- mysql_dump/mysql_dump.sql
+++ mysql_dump/mysql_dump.sql
@@ -31,12 +31,12 @@ CREATE TABLE `periodicals` (
   `title` varchar(100) NOT NULL,
   `description` text,
   `qty_per_year` int(11) NOT NULL,
-  `price` decimal(5,2) NOT NULL DEFAULT '0.00',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `periodicals_fk0` (`periodical_type`),
   CONSTRAINT `periodicals_fk0` FOREIGN KEY (`periodical_type`) REFERENCES `periodicals_types` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `periodicals` (
 
 LOCK TABLES `periodicals` WRITE;
 /*!40000 ALTER TABLE `periodicals` DISABLE KEYS */;
-INSERT INTO `periodicals` VALUES (1,'0190-8286','NEWSPAPPER','The Washington Post','The Washington Post (sometimes abbreviated as WaPo) is a major American daily newspaper published in Washington, D.C., with a particular emphasis on national politics and the federal government. It has the largest circulation in the Washington metropolitan area. Its slogan \"Democracy Dies in Darkness\" began appearing on its masthead in 2017. Daily broadsheet editions are printed for the District of Columbia, Maryland, and Virginia.',365,0.35,1),(2,'0362-4331','NEWSPAPPER','The New York Times','The New York Times (shortened as The Times and abbreviated as The NYT) is an American newspaper based in New York City with worldwide influence and readership.Founded in 1851, the paper has won 125 Pulitzer Prizes, more than any other newspaper. The Times is ranked 17th in the world by circulation and 2nd in the U.S. ',365,0.50,1),(3,'1','UNDEFINED','АБВГД','DELETE ME AGAIN',1,1.00,1),(4,'1234-123','UNDEFINED','TEST','TEst Discription',666,123.00,1),(5,'1234-5623','UNDEFINED','TEST','TEst Discription',666,123.00,1),(6,'1234-5678','UNDEFINED','TEST','TEst Discription',666,1.20,1),(7,'3587-8542','UNDEFINED','Marvel','Spider-Man',54,2.50,1),(8,'1234-4321','UNDEFINED','Enter title','Enter description',0,0.00,1),(9,'1234-4321','NEWSPAPPER','Перспектива','Перспективы нЭт',500,15.00,1),(10,'1234-4321','NEWSPAPPER','Советская белорусия','Как хорошо живется у нас в стране',365,100.00,1),(11,'1145-8545','UNDEFINED','ТЕперь мы periodicals','Enter descriptionon',15,20.00,1),(12,'1234-4321','UNDEFINED','djdghk','Enter descriptiodghkfghjdfn',0,0.00,1);
+INSERT INTO `periodicals` VALUES (1,'0190-8286','NEWSPAPPER','The Washington Post','The Washington Post (sometimes abbreviated as WaPo) is a major American daily newspaper published in Washington, D.C., with a particular emphasis on national politics and the federal government. It has the largest circulation in the Washington metropolitan area. Its slogan \"Democracy Dies in Darkness\" began appearing on its masthead in 2017. Daily broadsheet editions are printed for the District of Columbia, Maryland, and Virginia.',365,0.35,1),(2,'0362-4331','NEWSPAPPER','The New York Times','The New York Times (shortened as The Times and abbreviated as The NYT) is an American newspaper based in New York City with worldwide influence and readership.Founded in 1851, the paper has won 125 Pulitzer Prizes, more than any other newspaper. The Times is ranked 17th in the world by circulation and 2nd in the U.S. ',365,0.50,1),(3,'1992-3058','UNDEFINED','СБ. Беларусь сегодня','Белорусская общественно-политическая газета. Издаётся с августа 1927 года. Выходит 5 раз в неделю. Учредители — Администрация президента Республики Беларусь и редакционный совет. На декабрь 2013 года тираж — 400 614 экземпляров. Крупнейшее печатное СМИ страны. В последние годы выходит под брендом ≪СБ. Беларусь сегодня≫. ',270,0.37,1),(4,'5894-4521','UNDEFINED','Marvel Adventures','Marvel Adventures, formerly Marvel Age, was an imprint of Marvel Comics intended for younger audiences, including small children. Unlike the standard comics published by Marvel, which often take place in story arcs spanning several issues, each Marvel Adventures comic tells a standalone story. In April 2012 it was replaced by all new All Ages line tied to the Marvel Universe block on Disney XD.',12,17.00,1),(5,'3256-4521','UNDEFINED','Batman','Batman is an ongoing American comic book series featuring the DC Comics superhero Batman as its main protagonist. The character first appeared in Detective Comics #27 (cover dated May 1939). Batman proved to be so popular that a self-titled ongoing comic book series began publication with a cover date of Spring 1940. It was first advertised in early April 1940, one month after the first appearance of his new sidekick, Robin, the Boy Wonder. ',54,20.50,1),(6,'3697-5428','UNDEFINED','Наша нива','Начала выходить в Вильне с 10 (23) ноября 1906 по 7 августа 1915 на белорусском языке кириллицей и с первого номера до № 42, 18 (31) октября 1912) также белорусской латиницей, с заглавиями «Наша Ніва» и «Nasza Niwa».  Редакторы-издатели З. Вольский, А. Власов, И. Луцкевич. Печаталась в типографии Мартина Кухты. ',12,0.79,1),(7,'3587-8542','UNDEFINED','Marvel','Spider-Man',54,2.50,0),(8,'1234-4321','UNDEFINED','Enter title','Enter description',0,0.00,0),(9,'1234-4321','NEWSPAPPER','Перспектива','Перспективы нЭт',500,15.00,0),(10,'1234-4321','NEWSPAPPER','Советская белорусия','Как хорошо живется у нас в стране',365,100.00,0),(11,'1145-8545','UNDEFINED','ТЕперь мы periodicals','Enter descriptionon',15,20.00,0),(12,'1234-4321','UNDEFINED','djdghk','Enter descriptiodghkfghjdfn',0,0.00,0),(13,'5569-','UNDEFINED','Enter title','Enter description',0,0.00,0);
 /*!40000 ALTER TABLE `periodicals` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,20 +83,20 @@ DROP TABLE IF EXISTS `subscribes`;
 CREATE TABLE `subscribes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `periodicals_id` int(11) NOT NULL,
-  `total_price` decimal(5,2) NOT NULL DEFAULT '0.00',
-  `paid` decimal(5,2) NOT NULL DEFAULT '0.00',
-  `month_subscribed` int(11) NOT NULL DEFAULT '0',
+  `periodical_id` int(11) NOT NULL,
+  `total_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `paid` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `months_of_subscription` int(11) NOT NULL DEFAULT '0',
   `date_begin_subscribe` date NOT NULL,
   `date_end_subscribe` date NOT NULL,
   `payment_status` tinyint(1) NOT NULL DEFAULT '0',
-  `subscribe_status` tinyint(1) NOT NULL DEFAULT '0',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `subscribes_fk0` (`user_id`),
-  KEY `subscribes_fk1` (`periodicals_id`),
+  KEY `subscribes_fk1` (`periodical_id`),
   CONSTRAINT `subscribes_fk0` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `subscribes_fk1` FOREIGN KEY (`periodicals_id`) REFERENCES `periodicals` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `subscribes_fk1` FOREIGN KEY (`periodical_id`) REFERENCES `periodicals` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,6 +105,7 @@ CREATE TABLE `subscribes` (
 
 LOCK TABLES `subscribes` WRITE;
 /*!40000 ALTER TABLE `subscribes` DISABLE KEYS */;
+INSERT INTO `subscribes` VALUES (11,2,4,17.00,0.00,1,'2018-12-20','2019-01-20',0,1),(12,2,5,1107.00,0.00,12,'2018-12-20','2019-12-20',0,1);
 /*!40000 ALTER TABLE `subscribes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +134,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `phone` (`phone`),
   KEY `users_fk0` (`role_type`),
   CONSTRAINT `users_fk0` FOREIGN KEY (`role_type`) REFERENCES `users_roles` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,7 +143,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin','ADMIN','Admin','Adminovich','admin@admin.com','+375295556666','230025, Grodno, Kirova str., 1 ',100.00,1),(2,'nikola','tesla','CUSTOMER','Nikola','Tesla','nikolatesla@tut.by','+12123633200','10004, New York city',1000.00,1),(3,'god','god','CUSTOMER','super','user','super@user.com','+547862218','665478, Heaven',0.00,1),(4,'vasyapupkin','vasvas','ANONYMOUS','vasya','pupkin','vasua@pupkin.ko','+655546845','555666, Hell',0.00,0),(5,'customer','1234','CUSTOMER','Best','Customer','customer@thebest.com','+3752912345678','23000, Grodno, Kleckova 1',0.00,1);
+INSERT INTO `users` VALUES (1,'admin','admin','ADMIN','Admin','Adminovich','admin@admin.com','+375295556666','230025, Grodno, Kirova str., 1 ',100.00,1),(2,'nikola','tesla','CUSTOMER','Nikola','Tesla','nikolatesla@tut.by','+12123633200','10004, New York city',1000.00,1),(6,'testuser','12345678','CUSTOMER','Вася','Пупкин','vasilij.pupkin@gmail.com','+375296666666','230026 Grodno, Kleckova 26',0.00,1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,4 +188,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-19  1:04:56
+-- Dump completed on 2018-12-20 16:23:42
